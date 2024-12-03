2ff53c3eb5d79caa256bb72e1a7b063325823b41
Ziad Ibrahim <134080590+ZiadTantawy@users.noreply.github.com>
2024-12-03 17:38:40 +0200

    Fixed File Structure and started Admin dashboard 1

---
+++ HomePage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telecommunication_System
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void AdminLoginBtn(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminLogin.aspx");
+        }
+        protected void CustomerLoginBtn(object sender, EventArgs e)
+        {
+            Response.Redirect("CustomerLogin.aspx");
+        }
+    }
+}
