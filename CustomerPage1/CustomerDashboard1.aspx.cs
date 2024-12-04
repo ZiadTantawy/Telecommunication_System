d3c31e20573a16e282b544c9fc3420784c1f8555
maryam elsheikh <maryam.elsheikh.12@gmail.com>
2024-12-04 15:53:47 +0200

    customer

---
+++ CustomerPage1/CustomerDashboard1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telecommunication_System.CustomerPage1
+{
+    public partial class CustomerDashboard1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Serviceplans(object sender, EventArgs e)
+        {
+            Response.Redirect("ServicePlansView.aspx");
+        }
+        protected void Consumption(object sender, EventArgs e)
+        {
+            Response.Redirect("ConsumptionView.aspx");
+        }
+        protected void Otherplans(object sender, EventArgs e)
+        {
+            Response.Redirect("OtherPlansView.aspx");
+        }
+        protected void Monthlyusage(object sender, EventArgs e)
+        {
+            Response.Redirect("MonthlyUsageView.aspx");
+        }
+        protected void Cashback(object sender, EventArgs e)
+        {
+            Response.Redirect("CashbackView.aspx");
+        }
+    }
+}
